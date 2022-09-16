f4b592cb896db8403310bdbf998fb2f9640a3338
KiHoon Roh <started677@gmail.com>
2022-09-16 19:53:51 +0900

    setup: 초기세팅

---
+++ kh/src/redux/reducers/commentSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const commentSlice = createSlice({
+  name: 'comment',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {},
+});

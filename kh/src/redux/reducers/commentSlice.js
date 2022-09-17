fc6b4a7696061e345ccb340bc99e081afee4a91d
KiHoon Roh <started677@gmail.com>
2022-09-17 22:51:26 +0900

    feat: api, redux 기본 구조 구현

--- kh/src/redux/reducers/commentSlice.js
+++ kh/src/redux/reducers/commentSlice.js
@@ -1,10 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { deleteComment, getComment, modifyComment } from '@redux/actions/comment';
 
-const initialState = {};
+const initialState = {
+  comments: [],
+};
+
+function isPendingAction(action) {
+  return action.type.endsWith('/pending');
+}
+
+function isRejectedAction(action) {
+  return action.type.endsWith('/rejected');
+}
 
 export const commentSlice = createSlice({
   name: 'comment',
   initialState,
   reducers: {},
-  extraReducers: builder => {},
+  extraReducers: builder => {
+    builder
+      .addCase(getComment.fulfilled, (state, action) => {})
+      .addCase(deleteComment.fulfilled, (state, action) => {})
+      .addCase(modifyComment.fulfilled, (state, action) => {})
+      .addMatcher(isPendingAction, state => {})
+      .addMatcher(isRejectedAction, (state, action) => {});
+  },
 });

e87d411bd8f3ca87648b402838e3febe09573a01
wooyoon <dndbs14@gmail.com>
2022-09-17 23:09:54 +0900

    refactor: API 상수 env로 이동

---
+++ wy/src/api/deleteComment.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL
+export async function deleteComment(commentId){
+    await axios.delete(`${API_URL}/${commentId}`)
+    .then(function(response){console.log(response)})
+}

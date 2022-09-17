d52dfaa6e49305b926b1c6cad8326c3590b7d401
wooyoon <dndbs14@gmail.com>
2022-09-17 19:53:37 +0900

    refactor: API url 상수화
    API_URL 상수 파일 추가, getCommentList에 적용

--- wy/src/api/getCommentList.js
+++ wy/src/api/getCommentList.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import { API_URL } from '../constants/API_URL';
 export async function getCommentList() {
-  return axios.get('https://comment-api.herokuapp.com/comments').then(response => {
+  return axios.get(API_URL).then(response => {
     return response.data;
   });
 }

7990a43694994f683c0c3401e87077bf82967068
KiHoon Roh <started677@gmail.com>
2022-09-18 11:40:16 +0900

    ui: globalCSS 추가

--- kh/src/index.js
+++ kh/src/index.js
@@ -3,16 +3,22 @@ import { createRoot } from 'react-dom/client';
 import { store } from '@redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/globalStyle';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   </Provider>,
 );
